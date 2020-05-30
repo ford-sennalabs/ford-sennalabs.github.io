3f52b7a621b2f72f945d7dc1a287c0b0b8337627
pornsitbureenok <ford@sennalabs.com>
2020-05-31 04:43:49 +0700

    first initial react app 2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dd4a55f0debee346d097a6b4eda3c99.js"
+  "/precache-manifest.8e971f83947e74a6cafa69e51afda161.js"
 );
 
 self.addEventListener('message', (event) => {
