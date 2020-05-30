8844bb8aae65334037c7b374d16973755027f9db
pornsitbureenok <ford@sennalabs.com>
2020-05-31 04:22:25 +0700

    first initial react app

---
+++ precache-manifest.9dd4a55f0debee346d097a6b4eda3c99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4b72bf33f3f536cf7769adef080deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e43921a8f97fdb5c848d",
+    "url": "/static/css/main.b5ea828a.chunk.css"
+  },
+  {
+    "revision": "8a59c4d927d27b2abed5",
+    "url": "/static/js/2.8a1c111c.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.8a1c111c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43921a8f97fdb5c848d",
+    "url": "/static/js/main.104637be.chunk.js"
+  },
+  {
+    "revision": "32d7d43dee462ff06ebb",
+    "url": "/static/js/runtime-main.66ead37d.js"
+  },
+  {
+    "revision": "cd8693ef4159220bf079e6d9ebb20685",
+    "url": "/static/media/click.cd8693ef.svg"
+  }
+]);
